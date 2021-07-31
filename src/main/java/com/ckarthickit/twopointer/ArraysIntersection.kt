abf41594f3eca1e2f2be8610a88af47045f3872a
Karthick Chinnathambi <ckarthickit@gmail.com>
2021-07-31 14:13:05 +0530

    [twopointer] Updated Comments for ArrayIntersection

--- src/main/java/com/ckarthickit/twopointer/ArraysIntersection.kt
+++ src/main/java/com/ckarthickit/twopointer/ArraysIntersection.kt
@@ -1,5 +1,27 @@
 package com.ckarthickit.twopointer
 
+
+/***
+ * Given two integer arrays nums1 and nums2, return an array of their intersection.
+ * Each element in the result must be unique and you may return the result in any order.
+ *
+ *  Example1:
+ *  =========
+ *  Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ *  Output: [2]
+ *
+ *  Example2:
+ *  =========
+ *  Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ *  Output: [9,4]
+ *  Explanation: [4,9] is also accepted.
+ *
+ *  Solution:
+ *  The idea is to convert both arrays into sets,
+ *  and then iterate over the smallest set checking the presence of each element in the larger set.
+ *  Time complexity of this approach is O(n+m) in the average case.
+ */
+
 fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
     val smallerArray = if (nums1.size < nums2.size) nums1 else nums2
     val largerArray = if (smallerArray === nums2) nums1 else nums2
